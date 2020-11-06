04e0e56ed73101e7a5c8d671a312254a07ac3fdf
liangyouwen <layouwen@gmail.com>
2020-11-07 04:30:20 +0800

    创建Home页面

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Header/>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+  name: 'Home',
+  components: { Header },
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
