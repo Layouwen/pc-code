d918cfda9b5617b714f7c6286391db94317f144f
liangyouwen <layouwen@gmail.com>
2020-11-07 19:51:43 +0800

    完成Projects Case组件

---
+++ src/components/Home/ProjectsCase.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class='project-case'>
+    <div class="top">
+      <img src="http://www.seejoys.net.cn/Tpl/SJ_Home/conf/images/projects.png" alt="">
+      <!--      <div class="en">Projects Case</div>-->
+      <!--      <div class="cn">项目案例</div>-->
+    </div>
+    <div class="item-list">
+      <div class="item" v-for='item in itemList'>
+        <img :src="item.img" alt="">
+        <div class="title">{{item.title}}</div>
+        <div class="describe">{{item.describe}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'ProjectsCase',
+  setup() {
+    const itemList = [
+      {id: 1, img: 'http://www.seejoys.net.cn/pics/case/case_s_1541585552.png', title: '保时捷小程序', describe: '我是保时捷小程序'},
+      {id: 2, img: 'http://www.seejoys.net.cn/pics/case/case_s_1539933722.png', title: '通道商城', describe: '我是保时捷小程序'},
+      {id: 3, img: 'http://www.seejoys.net.cn/pics/case/case_s_1539933722.png', title: '天天洗衣', describe: '我是保时捷小程序'},
+      {id: 4, img: 'http://www.seejoys.net.cn/pics/case/case_s_1539933722.png', title: '洗车吧', describe: '我是保时捷小程序'},
+      {id: 5, img: 'http://www.seejoys.net.cn/pics/case/case_s_1539933722.png', title: '拉茶风味', describe: '我是保时捷小程序'},
+      {id: 6, img: 'http://www.seejoys.net.cn/pics/case/case_s_1539933722.png', title: '美团', describe: '我是保时捷小程序'},
+      {id: 7, img: 'http://www.seejoys.net.cn/pics/case/case_s_1539933722.png', title: '茜茜够', describe: '我是保时捷小程序'},
+      {id: 8, img: 'http://www.seejoys.net.cn/pics/case/case_s_1539933722.png', title: '探花', describe: '我是保时捷小程序'},
+    ];
+    return {itemList};
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.project-case {
+  width: 1200px;
+  padding: 80px 0;
+  margin: 0 auto;
+  
+  > .top {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 40px;
+  }
+  
+  > .item-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    
+    > .item {
+      margin-bottom: 40px;
+      
+      > .title, .describe {
+        text-indent: 1em;
+      }
+      
+      > .title {
+        margin: 10px 0;
+        font-size: 16px;
+        font-weight: bold;
+      }
+      
+      > .describe {
+        color: #6e6e6e;
+        font-size: 14px;
+      }
+    }
+    
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
   <Header/>
   <Swiper/>
-  <HomeCenter/>
+  <div class="line-wrapper">
+    <HomeCenter/>
+  </div>
+  <div class="line-wrapper">
+    <ProjectsCase/>
+  </div>
 </template>
 
-<script>
-import Header from '../components/Header.vue'
-import Swiper from '../components/Swiper.vue'
-import HomeCenter from '../components/Home/HomeCenter.vue'
+<script lang='ts'>
+import Header from '../components/Header.vue';
+import ProjectsCase from '../components/Home/ProjectsCase.vue';
+import Swiper from '../components/Swiper.vue';
+import HomeCenter from '../components/Home/HomeCenter.vue';
 
 export default {
   name: 'Home',
-  components: { Swiper, Header, HomeCenter }
-}
+  components: {ProjectsCase, Swiper, Header, HomeCenter},
+};
 </script>
+
+<style lang='scss' scoped>
+.line-wrapper {
+  border-bottom: 2px solid #e0e0e0;
+}
+</style>
