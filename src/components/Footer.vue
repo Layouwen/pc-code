3aa5fbfc8bf522bfee8b90b62e809bf17ff98c25
liangyouwen <layouwen@gmail.com>
2020-11-07 20:29:18 +0800

    添加footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    Copyright © 2020 layouwen.com .All rights reserved. 粤ICP备xxxxxx号-1
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style lang='scss' scoped>
+.footer {
+  display: flex;
+  justify-content: center;
+  padding: 50px;
+  font-size: 16px;
+  color: #fff;
+  background: #266a84;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,9 +10,11 @@
   <div class="line-wrapper" style='background: #f1f5f7;'>
     <ProjectProcess/>
   </div>
+  <Footer/>
 </template>
 
 <script lang='ts'>
+import Footer from '../components/Footer.vue';
 import Header from '../components/Header.vue';
 import ProjectProcess from '../components/Home/ProjectProcess.vue';
 import ProjectsCase from '../components/Home/ProjectsCase.vue';
@@ -21,7 +23,7 @@ import HomeCenter from '../components/Home/HomeCenter.vue';
 
 export default {
   name: 'Home',
-  components: {ProjectProcess, ProjectsCase, Swiper, Header, HomeCenter},
+  components: {Footer, ProjectProcess, ProjectsCase, Swiper, Header, HomeCenter},
 };
 </script>
 
