1cd57c9ed83261b2e09811bc26cc66d292d59e7d
liangyouwen <layouwen@gmail.com>
2020-11-07 20:11:03 +0800

    基本完成ProjectProcess组件

---
+++ src/components/Home/ProjectProcess.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class='project-process'>
+    <div class="top"><img src="http://www.seejoys.net.cn/Tpl/SJ_Home/conf/images/p.jpg" alt=""></div>
+    <ul class="title-list">
+      <li class="title">签订合同</li>
+      <li class="title">项目启动</li>
+      <li class="title">产品原型</li>
+      <li class="title">视觉图</li>
+      <li class="title">研发</li>
+      <li class="title">测试</li>
+      <li class="title">验收</li>
+      <li class="title">上线</li>
+    </ul>
+    <div class='img'></div>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'ProjectProcess',
+};
+</script>
+
+<style lang='scss' scoped>
+.project-process {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 1200px;
+  padding: 80px 0;
+  margin: 0 auto;
+  
+  > .top {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  > .title-list {
+    display: flex;
+    margin-top: 50px;
+    
+    > .title {
+      width: 135px;
+      text-align: center;
+      font-size: 22px;
+      color: #232729;
+    }
+  }
+  
+  > .img {
+    width: 1061px;
+    height: 148px;
+    background: url("http://www.seejoys.net.cn/Tpl/SJ_Home/conf/images/lc-icon.png") no-repeat 0 -40px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,21 @@
   <div class="line-wrapper">
     <ProjectsCase/>
   </div>
+  <div class="line-wrapper" style='background: #f1f5f7;'>
+    <ProjectProcess/>
+  </div>
 </template>
 
 <script lang='ts'>
 import Header from '../components/Header.vue';
+import ProjectProcess from '../components/Home/ProjectProcess.vue';
 import ProjectsCase from '../components/Home/ProjectsCase.vue';
 import Swiper from '../components/Swiper.vue';
 import HomeCenter from '../components/Home/HomeCenter.vue';
 
 export default {
   name: 'Home',
-  components: {ProjectsCase, Swiper, Header, HomeCenter},
+  components: {ProjectProcess, ProjectsCase, Swiper, Header, HomeCenter},
 };
 </script>
 
